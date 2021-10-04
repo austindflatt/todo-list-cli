38b0905f5e3fd9421f01328baa0c53ab8ba6a376
Austin Flatt <austindflatt@gmail.com>
2021-10-04 12:49:22 -0400

    added actions 1 & 2 into if statements

--- main.js
+++ main.js
@@ -4,7 +4,16 @@ console.log("Welcome to the To-Do List Manager Application!");
 
 while (true) {
     console.log("Your to-do list is empty.");
-    const action = prompt(`~ Select an action ~\n
+    const action = Number(prompt(`~ Select an action ~\n
     [1] Create a to-do item\n
-    [2] Complete a to-do item:`);
+    [2] Complete a to-do item:`));
+
+    if (action === 1) {
+        console.log("~ Creating a new to-do item ~");
+        let create = prompt("What is this to-do item called?");
+    } else if (action === 2) {
+        console.log("~ Completing a to-do item ~");
+        let complete = prompt("Which to-do item would you like to complete?");
+
+    }
 }
