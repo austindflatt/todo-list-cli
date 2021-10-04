fb897829063335d1b083439d6016e816873f62bf
Austin Flatt <austindflatt@gmail.com>
2021-10-04 16:01:05 -0400

    added array that contains boolean values for completed to-dos

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 const prompt = require('prompt-sync')({ sigint: true });
 
 console.log("Welcome to the To-Do List Manager Application!");
+let todos = [];
+let areTodosComplete = [false, true];
 
 while (true) {
-    console.log("Your to-do list is empty.");
+    if (todos.length === 0) {
+        console.log("Your to-do list is empty.");
+    } else {
+        console.log(`You have ${todos.length} to-do item(s).`);
+        for (let i = 0; i < todos.length; i++) {
+            console.log(`[incomplete] ${i + 1}. ${todos[i]}`)
+        }
+    }
     const action = Number(prompt(`~ Select an action ~\n
     [1] Create a to-do item\n
     [2] Complete a to-do item:`));
@@ -11,9 +20,12 @@ while (true) {
     if (action === 1) {
         console.log("~ Creating a new to-do item ~");
         let create = prompt("What is this to-do item called?");
+        todos.push(create);
     } else if (action === 2) {
         console.log("~ Completing a to-do item ~");
-        let complete = prompt("Which to-do item would you like to complete?");
-
+        let complete = Number(prompt("Which to-do item would you like to complete?"));
+            if (complete === todos) {
+                console.log("That item has been completed and removed from the list!");
+            }
     }
 }
