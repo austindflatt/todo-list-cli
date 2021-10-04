79007b12c399f184769231196d3e59c6513ff955
Austin Flatt <austindflatt@gmail.com>
2021-10-04 12:03:47 -0400

    add prompt message for action 1 & 2

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 const prompt = require('prompt-sync')({ sigint: true });
 
-console.log("Welcome to the To-Do List Manager Application!")
+console.log("Welcome to the To-Do List Manager Application!");
+
+while (true) {
+    console.log("Your to-do list is empty.");
+    const action = prompt(`~ Select an action ~\n
+    [1] Create a to-do item\n
+    [2] Complete a to-do item:`);
+}
